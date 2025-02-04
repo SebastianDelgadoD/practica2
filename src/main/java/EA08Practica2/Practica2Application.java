cf9cd96c09f81895b6211fdb464257bca4c78198
SebastianDelgado1011 <sebasld1011@gmail.com>
2025-02-03 18:14:47 -0600

    Practica2

---
+++ src/main/java/EA08Practica2/Practica2Application.java
@@ -0,0 +1,13 @@
+package EA08Practica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2Application.class, args);
+	}
+
+}
